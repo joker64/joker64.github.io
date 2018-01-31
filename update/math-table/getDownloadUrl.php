98fc44e33acd0cc5493358dc03febd68548d7a8b
yousef hosam <lastman985@yahoo.com>
2018-02-01 01:00:25 +0300

    Update getDownloadUrl.php

--- update/math-table/getDownloadUrl.php
+++ update/math-table/getDownloadUrl.php
@@ -1,5 +1,7 @@
 <GUP>
 <NeedToBeUpdated>no</NeedToBeUpdated>
 <Version>1.0</Version>
-<Location>https://joker64.github.io/update/math-table</Location>
+  <Location>
+    https://joker64.github.io/Math-Table.zip
+  </Location>
 </GUP>
