ebe48740f1cf7538a23906432fc50fb4beab417e
joker64 <lastman985@yahoo.com>
2018-08-25 09:38:47 +0300

    Update

--- update/math-table/getDownloadUrl.php
+++ update/math-table/getDownloadUrl.php
@@ -1,7 +1,5 @@
 <GUP>
 <NeedToBeUpdated>no</NeedToBeUpdated>
 <Version>1.0</Version>
-  <Location>
-    https://joker64.github.io/Math-Table.zip
-  </Location>
+<Location>https://joker64.github.io/update/math-table</Location>
 </GUP>
